aa44d47b885e79087833ea89ea677bfd289e89ab
Danila <danilasklovskii@gmail.com>
2023-12-07 19:15:58 +0100

    v2

--- volume_control.py
+++ volume_control.py
@@ -5,7 +5,12 @@ import mediapipe as mp
 cap = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+)
 
 mp_drawing = mp.solutions.drawing_utils
 
@@ -14,33 +19,35 @@ while True:
     if not ret:
         break
 
-    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BRG2RGB)
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(image_rgb)
 
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hands_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    
-    index_finger_y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
-    thumb_y = hand_landmarks.landmark[mp_hands.HandLandmark.IHUMB_TIP].y
 
-    if index_finger_y < thumb_y:
-        hand_gesture = 'pointing up'
-    elif index_finger_y > thumb_y:
-        hand_gesture = 'pointing down'
-    else:
-        hand_gesture = 'other'
+            index_finger_y = hand_landmarks.landmark[
+                mp_hands.HandLandmark.INDEX_FINGER_TIP
+            ].y
+            thumb_y = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
 
-    if hand_gesture == 'pointing up':
-        pyautogui.press('volumeup')
-    elif hand_gesture == 'pointing down':
-        pyautogui.press('volume down')
-    
-    cv2.imshow('Hand Gesture', frame)
+            if index_finger_y < thumb_y:
+                hand_gesture = "pointing up"
+            elif index_finger_y > thumb_y:
+                hand_gesture = "pointing down"
+            else:
+                hand_gesture = "other"
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            if hand_gesture == "pointing up":
+                pyautogui.press("volumeup")
+            elif hand_gesture == "pointing down":
+                pyautogui.press("volumedown")
+
+            cv2.imshow("Hand Gesture", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
 cap.release()
 cv2.destroyAllWindows()
